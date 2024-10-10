ff1f3221fbb8ca472cca5b66b03ee453abd94bf7
Denis <bigdramashow730@gmail.com>
2024-10-10 23:24:16 +0300

    init

---
+++ HWs/HW_01/Client_hw_01/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Sockets;
+using System.Text;
+internal class Program
+{
+    static async Task Main()
+    {
+
+        // Task 1
+        //Разработайте набор консольных приложений. 
+        //Первое приложение: серверное приложение, которое на запросы клиента возвращает текущее время или дату на сервере. 
+        //Второе приложение: клиентское приложение, запрашивающее дату или время.
+        //Пользователь с клавиатуры определяет, что нужно запросить. После отсылки даты или времени сервер разрывает соединение. Клиентское приложение отображает полученные данные.
+
+        //==================================
+
+        //  Task 2
+        //Создайте сервер, который принимает запрос от клиента, вычисляет квадратный корень числа, отправленного клиентом, и возвращает результат
+
+
+        while (true)
+        {
+            Console.WriteLine($"\nRequest to server: (time,date, (int)number or exit)");
+            string request = Console.ReadLine();
+            if(request.ToLower() == "exit")
+                break;
+            ConnectToServer(request);   // Будет постоянно коннектиться и передавать запрос, надо будет сделать решение.
+
+        }
+    }
+    static void ConnectToServer(string request)
+    {
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8888);
+            Console.WriteLine("Подключено к серверу...");
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] data = Encoding.UTF8.GetBytes(request);
+            stream.Write(data, 0, data.Length);
+
+
+            data = new byte[512];
+            StringBuilder response = new StringBuilder();
+            int bytes;
+
+            do
+            {
+                bytes = stream.Read(data, 0, data.Length);
+                response.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            } while (stream.DataAvailable);
+
+            Console.WriteLine($"Server response: {response}");
+
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+    }
+}
+
---
+++ HWs/HW_01/TcpServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StartServer();
+        Console.ReadLine();
+    }
+
+    public static void StartServer()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8888;
+        TcpListener listener = new TcpListener(ipAddress, port);
+
+        try
+        {
+            listener.Start();
+            Console.WriteLine("Сервер запущен...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Новый клиент подключен.");
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[512];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string request = Encoding.UTF8.GetString(buffer,0,bytesRead).ToLower();
+
+
+                // Достаем EndPoint Клиента
+                IPEndPoint clientEndPoint = client.Client.RemoteEndPoint as IPEndPoint;
+                string clientInfo = clientEndPoint != null ? $"{clientEndPoint.Address}:{clientEndPoint.Port}" : "Неизвестный клиент";
+
+
+                Console.WriteLine($"From client: {clientInfo} \nRequest: {request}");   // Надо сделать красиво
+
+
+                string response = null;
+
+                if(request == "time")
+                {
+                    response = DateTime.Now.ToLongTimeString();
+                }
+                else if (request == "date")
+                {
+                    response= DateTime.Now.ToLongDateString();
+                }
+                else if (double.TryParse(request, out double num ))
+                {
+                    response = Math.Sqrt(num).ToString();
+                }
+                else
+                {
+                    response = "Invalid request";
+                }
+
+                byte[] data = Encoding.UTF8.GetBytes(response);
+                stream.Write(data, 0, data.Length);
+
+                client.Close();   
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
